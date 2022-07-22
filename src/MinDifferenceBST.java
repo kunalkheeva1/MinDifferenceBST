69f1d670423d56e19d777675df7e305f5a51869d
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-22 01:48:09 -0700

    coded the node classs

--- src/MinDifferenceBST.java
+++ src/MinDifferenceBST.java
@@ -1,5 +1,43 @@
 public class MinDifferenceBST {
+//node
+    class Node{
+        int val;
+        Node left;
+        Node right;
+        Node(int val, Node left, Node right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static int minDifferenceBST(Node root, int target){
+        if(root== null) return -1;
+        int ans = Integer.MAX_VALUE;
+        Node current = root;
+        while(current != null){
+            int difference = current.val - target;
+            ans = Math.min(ans, Math.abs(difference));
+
+            if(difference> 0){
+                current = current.left;
+            }else if(difference <0){
+                current = current.right;
+            }else if(difference == 0){
+                return 0;
+            }
+
+
+
+        }return ans;
+
+
+    }
+
+
+
+
     public static void main(String[] args) {
-        
+
     }
 }
