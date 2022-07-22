b3dc61183a04c290b6864afc987a5e0e7aae655f
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-22 01:52:34 -0700

    added the comments for the future references

--- src/MinDifferenceBST.java
+++ src/MinDifferenceBST.java
@@ -12,13 +12,19 @@ public class MinDifferenceBST {
     }
 //method for min difference
     static int minDifferenceBST(Node root, int target){
+        //return -1 if the root is null
         if(root== null) return -1;
+
         int ans = Integer.MAX_VALUE;
         Node current = root;
+        // until the current becomes null
         while(current != null){
+            //diffrence is the data of the node - target
             int difference = current.val - target;
             ans = Math.min(ans, Math.abs(difference));
-
+            //keep updating the answer by checking the differences
+            // if difference is more then move to left side to find the smaller elements
+            // else move accordingly and update the answer
             if(difference> 0){
                 current = current.left;
             }else if(difference <0){
